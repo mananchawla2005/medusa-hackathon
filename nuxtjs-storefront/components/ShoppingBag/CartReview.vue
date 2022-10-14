dada603d1b2c5cf8a02494a68384416d3d508879
Manan Chawla <mananapeejay@gmail.com>
2022-10-14 19:42:47 +0000

    initial2

---
+++ nuxtjs-storefront/components/ShoppingBag/CartReview.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-white rounded-md shadow px-8 py-6 w-full sticky top-28">
+    <h3 class="font-semibold mb-4">
+      Order Summary
+    </h3>
+
+    <totals :item="$store.state.cart.cart" />
+
+    <nuxt-link to="/checkout">
+      <div class="btn-ui w-full text-center">
+        Checkout
+      </div>
+    </nuxt-link>
+  </div>
+</template>
---
+++ nuxtjs-storefront/nuxt.config.js
@@ -0,0 +1,67 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+  server: {
+    port: 8000 // default: 3000
+  },
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxtjs-storefront',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: '~/plugins/nuxtInit.client.js', mode: 'client' },
+    { src: '~/plugins/directives.client.js', mode: 'client' }
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    'nuxt-windicss',
+    '@braid/vue-formulate/nuxt'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    ['nuxt-stripe-module', {
+      publishableKey: process.env.PUBLISHABLE_KEY
+    }]
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: process.env.MEDUSA_BACKEND_URL || 'http://localhost:9000/store'
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
